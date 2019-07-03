11d1711e68702544c1f528cd9f54d792902d8d55
Stefan Schoettelndreyer <sschoettelndreyer@codevise.de>
2019-07-03 18:18:22 +0200

    Adds package.json

--- www/useragent.js
+++ www/useragent.js
@@ -1,18 +1,21 @@
-var noop = function() { };
+var noop = function () { };
 
 var userAgent = {
-    get: function (success, fail) {
-        if (success) {
-           cordova.exec(success, fail || noop, "UserAgent", "get", []);
-         } else {
-           return false;
-        }
-    },
-    set: function (userAgentString || '', success, fail) {
-        cordova.exec(success || noop, fail || noop, "UserAgent", "set", [userAgentString]);
-    },
-    reset: function (success, fail) {
-        cordova.exec(success || noop, fail || noop, "UserAgent", "reset", []);
+  get: function (success, fail) {
+    if (success) {
+      cordova.exec(success, fail || noop, "UserAgent", "get", []);
+    } else {
+      return false;
     }
+  },
+
+  set: function (userAgentString, success, fail) {
+    userAgentString = userAgentString || '';
+    cordova.exec(success || noop, fail || noop, "UserAgent", "set", [userAgentString]);
+  },
+  
+  reset: function (success, fail) {
+    cordova.exec(success || noop, fail || noop, "UserAgent", "reset", []);
+  }
 };
 module.exports = userAgent;
